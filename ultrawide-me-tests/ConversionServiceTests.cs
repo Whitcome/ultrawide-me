8de504cf66fff549348b70c9b9c39227a8680c4c
Dave <contact@davidwhitcome.com>
2020-07-09 23:24:50 -0500

    ConversionServiceTests

--- ultrawide-me-tests/ConversionServiceTests.cs
+++ ultrawide-me-tests/ConversionServiceTests.cs
@@ -4,10 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ultrawide_me_tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ConversionServiceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ProcessFile_2560x1440_Return()
         {
         }
     }
