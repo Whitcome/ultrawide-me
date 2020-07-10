436de6985c215e8e4536752a2475055ae3055be0
Dave <contact@davidwhitcome.com>
2020-07-09 23:07:51 -0500

    width and height - textboxes and labels

--- ultrawide-me/Form1.Designer.cs
+++ ultrawide-me/Form1.Designer.cs
@@ -34,7 +34,10 @@
             this.openFileNameLabel = new System.Windows.Forms.Label();
             this.convertButton = new System.Windows.Forms.Button();
             this.saveButton = new System.Windows.Forms.Button();
-            this.widthMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog
@@ -93,16 +96,41 @@
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.ShowSaveDialog);
             // 
-            // widthMaskedTextBox
+            // widthTextBox
             // 
-            this.widthMaskedTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.widthMaskedTextBox.Location = new System.Drawing.Point(312, 33);
-            this.widthMaskedTextBox.Mask = "000000";
-            this.widthMaskedTextBox.Name = "widthMaskedTextBox";
-            this.widthMaskedTextBox.PromptChar = ' ';
-            this.widthMaskedTextBox.Size = new System.Drawing.Size(100, 24);
-            this.widthMaskedTextBox.TabIndex = 4;
-            this.widthMaskedTextBox.ValidatingType = typeof(int);
+            this.widthTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.widthTextBox.Location = new System.Drawing.Point(324, 40);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(65, 23);
+            this.widthTextBox.TabIndex = 4;
+            this.widthTextBox.Text = "3440";
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightTextBox.Location = new System.Drawing.Point(405, 40);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(69, 23);
+            this.heightTextBox.TabIndex = 5;
+            this.heightTextBox.Text = "1440";
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.Location = new System.Drawing.Point(321, 24);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(35, 13);
+            this.widthLabel.TabIndex = 6;
+            this.widthLabel.Text = "Width";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Location = new System.Drawing.Point(402, 24);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(38, 13);
+            this.heightLabel.TabIndex = 7;
+            this.heightLabel.Text = "Height";
             // 
             // Form1
             // 
@@ -110,7 +138,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(800, 170);
-            this.Controls.Add(this.widthMaskedTextBox);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.widthLabel);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.widthTextBox);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.convertButton);
             this.Controls.Add(this.openFileNameLabel);
@@ -130,7 +161,10 @@
         private System.Windows.Forms.Label openFileNameLabel;
         private System.Windows.Forms.Button convertButton;
         private System.Windows.Forms.Button saveButton;
-        private System.Windows.Forms.MaskedTextBox widthMaskedTextBox;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.Label heightLabel;
     }
 }
 
